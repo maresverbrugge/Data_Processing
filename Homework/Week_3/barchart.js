2b4c3eaf8aeed4ad8969f2c920099cd10e674fc0
maresverbrugge <maressirou@gmail.com>
2018-02-23 17:51:33 +0100

    add first files of week 3 assignment

---
+++ Homework/Week_3/barchart.js
@@ -0,0 +1,16 @@
+/*This is an old test file, I wrote all my new code in the .html file
+I know it's more neat to split it into a css and js as well, 
+I will try to do that next assignment*/
+
+// var data = [4, 8, 15, 16, 23, 42];
+
+// var x = d3.scale.linear()
+//     .domain([0, d3.max(data)])
+//     .range([0, 420]);
+
+// d3.select(".chart")
+//   .selectAll("div")
+//     .data(data)
+//   .enter().append("div")
+//     .style("width", function(d) { return x(d) + "px"; })
+//     .text(function(d) { return d; });
