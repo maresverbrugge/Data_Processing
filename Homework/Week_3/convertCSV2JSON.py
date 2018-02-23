b7544673cf5e6370d4ace817c3ab8457d2241a04
maresverbrugge <maressirou@gmail.com>
2018-02-23 17:54:56 +0100

    add other files of week 3 assignment

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -5,9 +5,15 @@ import json
 csvfile = open('CSV_data.csv', 'r')
 jsonfile = open('JSON_data.json', 'w')
 
-fieldnames = ("STN", "Date", "View")
+fieldnames = ("STN", "Datum", "View")
 reader = csv.DictReader(csvfile, fieldnames)
 # fill json file
+a = []
 for row in reader:
 	if row["STN"][0] is not "#":
-		json.dump(row, jsonfile, indent = 4)
+		dicty = {}
+		dicty['Datum'] = row["Datum"]
+		dicty["View"] = row["View"][0:5]
+		a.append(dicty)
+		
+json.dump(a, jsonfile, indent = 4)
