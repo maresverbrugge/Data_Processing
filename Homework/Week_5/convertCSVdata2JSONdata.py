9fad014d81a08328eb8c5349b53a28a5e836b47a
maresverbrugge <maressirou@gmail.com>
2018-03-09 17:06:09 +0100

    added first versions of week 5 files

--- Homework/Week_5/convertCSVdata2JSONdata.py
+++ Homework/Week_5/convertCSVdata2JSONdata.py
@@ -7,15 +7,29 @@ jsonfile = open('JSON_data.json', 'w')
 
 fieldnames = ("STN", "Datum", "AverageTemp", "MinimumTemp", "MaximumTemp")
 reader = csv.DictReader(csvfile, fieldnames)
+
+# make empty dict
+dicty = {}
+# make 2 years with 3 dicts (min, max, average) within 1 list each
+dicty["1995"] = [{"Name": "MinimumTemp", "value": []}, {"Name": "MaximumTemp", "value": []}, {"Name": "AverageTemp", "value":[]}]
+dicty["2017"] = [{"Name": "MinimumTemp", "value": []}, {"Name": "MaximumTemp", "value": []}, {"Name": "AverageTemp", "value":[]}]
+
 # fill json file
-a = []
+temps = ["MinimumTemp", "MaximumTemp", "AverageTemp"]
 for row in reader:
-	if row["STN"][0] is not "#":
-		dicty = {}
-		dicty["Datum"] = row["Datum"]
-		dicty["AverageTemp"] = row["AverageTemp"]
-		dicty["MinimumTemp"] = row["MinimumTemp"]
-		dicty["MaximumTemp"] = row["MaximumTemp"]
-		a.append(dicty)
-		
-json.dump(a, jsonfile, indent = 4)
+	if row["STN"][0] is not "#" and row["Datum"]:
+		if row["Datum"][0:4] == "1995":
+			count = 0
+			for temp in temps:
+				current_point = {"date": row["Datum"], "value": row[temp]}
+				dicty["1995"][count]["value"].append(current_point)
+				count += 1
+
+		else:
+			count = 0
+			for temp in temps:
+				current_point = {"date": row["Datum"], "value": row[temp]}
+				dicty["2017"][count]["value"].append(current_point)
+				count += 1
+json.dump(dicty, jsonfile, indent = 4)
+
--- Homework/Week_5/d3line.js
+++ Homework/Week_5/d3line.js
@@ -1,101 +1,122 @@
+/* 
+Mares Verbrugge
+10519505
+*/
+
 window.onload = function() {
+
 	// costom the dimensions of the canvas
-	var margin = {top: 20, right: 30, bottom: 30, left: 20},
+	var margin = {top: 20, right: 120, bottom: 30, left: 40},
 		width = 960 - margin.left - margin.right,
 		height = 500 - margin.top - margin.bottom;
 	
-	var svg = d3.select(".scatterplot")
+	var g = d3.select(".linegraph")
 	.attr("width", width + margin.left + margin.right)
 	.attr("height", height + margin.top + margin.bottom)
 	.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.bottom + ")");
 
+	var color = d3.schemeCategory10;
+
 	// set the ranges of the canvas
-	var x = d3.scale.linear().range([0, width]);
-	var y = d3.scale.linear().range([height, 0]);
+	var x = d3.scaleTime().range([0, width]);
+	var y = d3.scaleLinear().range([height, 0]);
+	var z = d3.scaleOrdinal(color);
 
-	// set colors
-	var color = d3.scale.category10();
+	// make line function
+	var line = d3.line()
+		.x(function(d) { return x(d.date); })
+    	.y(function(d) { return y(d.value); });
+	
+	var parseTime = d3.timeParse("%Y%m%d");
 
-	// define and add the axis
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom");
+	// load data
+	d3.json("JSON_data.json", function(error, data) {
 
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left");
-
-	// load data separeted by ';'
-	d3.dsv(';')("scatterplotdata_2.csv", function(error, data) {
+		// throw error if necessary
 		if (error) throw error;
+	
+		var color_domain = [];
+		var min_temp = 10000;
+		var max_temp = -10000;
+		data["1995"].forEach(function(d) {
 
-		data.forEach(function(d) {
-			d.AverageLifeExpectancy = +d.AverageLifeExpectancy;
-			d.AverageWellbeing = +d.AverageWellbeing;
+			// make list color_domain for three different lines with colors
+			color_domain.push(d.Name);
+			d["value"].forEach(function(i) {
+
+				// make data usable for chart
+				i["value"] = +i["value"]
+
+				// find minimum and maximum temperatures (for y-axis)
+				if (i["value"] < min_temp) {
+					min_temp = i["value"]
+				}
+				if (i["value"] > max_temp) {
+					max_temp = i["value"]
+				}
+
+				// make data usable for chart
+				i["date"] = parseTime(i.date)
+			});
+
+
 		});
 
-		// scale the range of the data
-		// show possible values of wellbeing eventhough data doesn't contain those values
-		// (no country scores wellbeing > 8.5)
-		min_max_wellbeing = [0, 10]
-		x.domain(d3.extent(data, function(d) { return d.AverageLifeExpectancy; })).nice();
-		y.domain(min_max_wellbeing).nice();
+		// set domains
+		x.domain([data["1995"][0]["value"][0]["date"], data["1995"][0]["value"][364]["date"]]);
+		y.domain([min_temp, max_temp]);
+		z.domain(color_domain)
 
-	svg.append("g")
-		.attr("class", "x axis")
-		.attr("transform", "translate(" + margin.left + "," + height + ")")
-		.call(xAxis)
-	.append("text")
-		.attr("class", "label")
-		.attr("x", width)
-		.attr("y", -6)
-		.style("text-anchor", "end")
-		.text("Average Life Expectancy (years)");
+		// add x axis
+		g.append("g")
+			.attr("class", "axis x_axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x));
 
-  	svg.append("g")
-  		.attr("class", "y axis")
-  		.attr("transform", "translate(" + margin.left + ",0)")
-  		.call(yAxis)
-    .append("text")
-		.attr("class", "label")
-		.attr("transform", "rotate(-90)")
-		.attr("x", 0)
-		.attr("y", 10)
-		.attr("dy", ".71em")
-		.style("text-anchor", "end")
-		.text("Average Wellbeing (0-10)")
+		// add y axis
+		g.append("g")
+			.attr("class", "axis y_axis")
+			.call(d3.axisLeft(y))
+			.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 6)
+			.attr("dy", "0.71em")
+			.attr("fill", "#000")
+			.text("Temperature (0.1) in ºC");
 
-	// add the scatterplot
-	svg.selectAll(".dot")
-		.data(data)
-	.enter().append("circle")
-		.attr("class", "dot")
-		.attr("r", 3.5)
-		.attr("cx", function(d) { return x(d.AverageLifeExpectancy); })
-		.attr("cy", function(d) { return y(d.AverageWellbeing); })
-		.style("fill", function(d) { return color(d.Region); });
+		// add three g's voor each line
+		var temp_lines  = g.selectAll(".lines")
+			.data(color_domain)
+			.enter().append("g")
+			.attr("class", "lines");
 
-	// add legend	
-	var legend = svg.selectAll(".legend")
-		.data(color.domain())
-	.enter().append("g")
-		.attr("class", "legend")
-		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+		// draw lines
+		temp_lines.append("path")
+			.attr("class", "line")
+			.attr("d", function(d, i) { return line(data["1995"][i]["value"]); })
+			.style("stroke", function(d) { return z(d); })
 
-	var legendRectSize = 18;
+		// add text to lines to explain what you are looking at (sort of legend)
+		temp_lines.append("text")
+			.datum(function(d, i) { console.log(i); return {id: d, value: data["1995"][i]["value"][364]}; })
+			.attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.value) + ")"; })
+			.attr("x", 3)
+			.attr("dy", "0.35em")
+			.style("fill", function(d) { return z(d.id); })
+			.text(function(d) { return d.id; });
 
-	legend.append("rect")
-		.attr("x", margin.left + 240)
-		.attr("width", legendRectSize)
-		.attr("height", legendRectSize)
-		.style("fill", color);
-
-	legend.append("text")
-		.attr("x", margin.left + 230)
-		.attr("y", 9)
-		.attr("dy", ".35em")
-		.style("text-anchor", "end")
-		.text(function(d) { return d; });
-});
-}
+		// add title to graph
+		g.append("text")
+			.attr("x", (width / 2))             
+			.attr("y", 0 - (margin.top / 2))
+			.attr("text-anchor", "middle")  
+			.style("font-size", "16px") 
+			.style("font-weight", "bold") 
+			.text("Temperatures in 1995 measured by KNMI in De Bilt, The Netherlands");		
+		
+		// TO DO:
+		// 1. create tooltip/interactivity
+		// 2. make update/data of year 2017
+	});
+};
